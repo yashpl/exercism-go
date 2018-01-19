d892c9b9073ffd62720f4c0d50574b38ed11349e
yash <yashpaliwal14@gmail.com>
2018-01-19 14:32:35 +0530

    Reverse-string

--- reverse-string/string.go
+++ reverse-string/string.go
@@ -1,18 +1,14 @@
 package reverse
 
-import (
-  "strings"
-)
+
 
 //reverse a string
 
 func String(input string) string {
 
-  length := len(input)
-  buffer := make([]string,length)
-  j,i:= 0,length
-  for ; i > 0 ; i,j=i-1,j+1{
-    buffer[j] = string(input[i])
+var str string
+  for _,i:= range input {
+    str = string(i) + str
     }
-    return strings.Join(buffer,"")
+    return str
 }
